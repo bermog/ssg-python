35daeb31ba89680fc5be2367732d366f75bdb56f
Damian Bermejo <code@bermog.com>
2024-12-23 22:43:53 +0100

    Add TextNode class

---
+++ src/main.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+from textnode import TextNode, TextType
+
+
+def main():
+    node = TextNode("This is a text node", TextType.BOLD, "https://boot.dev")
+    print(node)
+
+main()
