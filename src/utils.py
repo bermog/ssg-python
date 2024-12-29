e06d4ef91ebd49321941dfed773e48477eb94372
Damian Bermejo <code@bermog.com>
2024-12-29 23:58:31 +0100

    Update name of lambda parameters in markdown_to_blocks util

--- src/utils.py
+++ src/utils.py
@@ -110,6 +110,6 @@ class Utils:
     @staticmethod
     def markdown_to_blocks(markdown):
         blocks = markdown.split("\n\n")
-        blocks = list(map(lambda p: p.strip(), blocks))
-        blocks = list(filter(lambda p: len(p) > 0, blocks))
+        blocks = list(map(lambda block: block.strip(), blocks))
+        blocks = list(filter(lambda block: len(block) > 0, blocks))
         return blocks
