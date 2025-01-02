4e959a44fae9e100c18b7bd6f4d283be6267701b
Damian Bermejo <code@bermog.com>
2025-01-02 21:03:42 +0100

    Refactor inline Utils methods and tests into their own files

---
+++ src/test_inlineutils.py
@@ -0,0 +1,145 @@
+import unittest
+
+from textnode import TextNode, TextType
+from inlineutils import InlineUtils
+
+
+class TestInlineUtils(unittest.TestCase):
+    def test_text_to_textnodes(self):
+        text = "This is **bold** text with *some italic* and a `code block`, with "
+        text += "![a cat image](https://imgur.com/NUyttbn) and a "
+        text += "[link to google](https://google.com)"
+        expected = [
+            TextNode("This is ", TextType.NORMAL),
+            TextNode("bold", TextType.BOLD),
+            TextNode(" text with ", TextType.NORMAL),
+            TextNode("some italic", TextType.ITALIC),
+            TextNode(" and a ", TextType.NORMAL),
+            TextNode("code block", TextType.CODE),
+            TextNode(", with ", TextType.NORMAL),
+            TextNode("a cat image", TextType.IMAGE, "https://imgur.com/NUyttbn"),
+            TextNode(" and a ", TextType.NORMAL),
+            TextNode("link to google", TextType.LINK, "https://google.com"),
+        ]
+        actual = InlineUtils.text_to_textnodes(text)
+        self.assertEqual(expected, actual)
+
+    def test_bold_split_nodes_delimiter(self):
+        node1 = TextNode("Some **bold text here** and more text", TextType.NORMAL)
+        node2 = TextNode("**Bold text** and **more bold text**", TextType.NORMAL)
+        expected = [
+            TextNode("Some ", TextType.NORMAL),
+            TextNode("bold text here", TextType.BOLD),
+            TextNode(" and more text", TextType.NORMAL),
+            TextNode("Bold text", TextType.BOLD),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("more bold text", TextType.BOLD),
+        ]
+        actual = InlineUtils.split_nodes_delimiter([node1, node2], "**", TextType.BOLD)
+        self.assertEqual(expected, actual)
+
+    def test_italic_split_nodes_delimiter(self):
+        node1 = TextNode("Some *italic text here* and more text", TextType.NORMAL)
+        node2 = TextNode("*Italic text* and *more italic text*", TextType.NORMAL)
+        expected = [
+            TextNode("Some ", TextType.NORMAL),
+            TextNode("italic text here", TextType.ITALIC),
+            TextNode(" and more text", TextType.NORMAL),
+            TextNode("Italic text", TextType.ITALIC),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("more italic text", TextType.ITALIC),
+        ]
+        actual = InlineUtils.split_nodes_delimiter([node1, node2], "*", TextType.ITALIC)
+        self.assertEqual(expected, actual)
+
+    def test_code_split_nodes_delimiter(self):
+        node1 = TextNode("Some `code here` and more text", TextType.NORMAL)
+        node2 = TextNode("`Code` and `more code`", TextType.NORMAL)
+        expected = [
+            TextNode("Some ", TextType.NORMAL),
+            TextNode("code here", TextType.CODE),
+            TextNode(" and more text", TextType.NORMAL),
+            TextNode("Code", TextType.CODE),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("more code", TextType.CODE),
+        ]
+        actual = InlineUtils.split_nodes_delimiter([node1, node2], "`", TextType.CODE)
+        self.assertEqual(expected, actual)
+
+    def test_extract_markdown_links(self):
+        text = """
+            first url [to google](https://google.com) and another url
+            [ddg](https://duckduckgo.com/)
+        """
+        expected = [
+            ("to google", "https://google.com"),
+            ("ddg", "https://duckduckgo.com/"),
+        ]
+        actual = InlineUtils.extract_markdown_links(text)
+        self.assertEqual(expected, actual)
+
+    def test_extract_markdown_images(self):
+        text = """
+            here is a gif ![cat gif](https://imgur.com/NUyttbn) and another one
+            ![squirrel gif](https://imgur.com/G9LgOkg)
+        """
+        expected = [
+            ("cat gif", "https://imgur.com/NUyttbn"),
+            ("squirrel gif", "https://imgur.com/G9LgOkg"),
+        ]
+        actual = InlineUtils.extract_markdown_links(text)
+        self.assertEqual(expected, actual)
+
+    def test_split_nodes_link(self):
+        node1 = TextNode(
+            "[Google](https://google.com) and some text",
+            TextType.NORMAL,
+        )
+        node2 = TextNode(
+            "This is a link [to google](https://google.com) and some text",
+            TextType.NORMAL,
+        )
+        node3 = TextNode(
+            "This is a link [to google](https://google.com) and [to ddg](https://duckduckgo.com)",
+            TextType.NORMAL,
+        )
+        expected = [
+            TextNode("Google", TextType.LINK, "https://google.com"),
+            TextNode(" and some text", TextType.NORMAL),
+            TextNode("This is a link ", TextType.NORMAL),
+            TextNode("to google", TextType.LINK, "https://google.com"),
+            TextNode(" and some text", TextType.NORMAL),
+            TextNode("This is a link ", TextType.NORMAL),
+            TextNode("to google", TextType.LINK, "https://google.com"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("to ddg", TextType.LINK, "https://duckduckgo.com"),
+        ]
+        actual = InlineUtils.split_nodes_link([node1, node2, node3])
+        self.assertEqual(expected, actual)
+
+    def test_split_nodes_image(self):
+        node1 = TextNode(
+            "![Cat](https://imgur.com/NUyttbn) and some text",
+            TextType.NORMAL,
+        )
+        node2 = TextNode(
+            "This is ![a cat](https://imgur.com/NUyttbn) and some text",
+            TextType.NORMAL,
+        )
+        node3 = TextNode(
+            "This is ![a cat](https://imgur.com/NUyttbn) and ![a squirrel](https://imgur.com/G9LgOkg)",
+            TextType.NORMAL,
+        )
+        expected = [
+            TextNode("Cat", TextType.IMAGE, "https://imgur.com/NUyttbn"),
+            TextNode(" and some text", TextType.NORMAL),
+            TextNode("This is ", TextType.NORMAL),
+            TextNode("a cat", TextType.IMAGE, "https://imgur.com/NUyttbn"),
+            TextNode(" and some text", TextType.NORMAL),
+            TextNode("This is ", TextType.NORMAL),
+            TextNode("a cat", TextType.IMAGE, "https://imgur.com/NUyttbn"),
+            TextNode(" and ", TextType.NORMAL),
+            TextNode("a squirrel", TextType.IMAGE, "https://imgur.com/G9LgOkg"),
+        ]
+        actual = InlineUtils.split_nodes_image([node1, node2, node3])
+        self.assertEqual(expected, actual)
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,8 @@
 import re
-from itertools import zip_longest
 
+from htmlnode import HTMLNode
 from leafnode import LeafNode
-from textnode import TextNode, TextType
+from textnode import TextType
 
 
 class Utils:
@@ -25,87 +25,6 @@ class Utils:
                 )
             case _:
                 raise Exception("Invalid TextNode type")
-
-    @staticmethod
-    def split_nodes_delimiter(old_nodes, delimiter, text_type):
-        # Delimiter will be used in a regex, so we must escape all "*"
-        if "*" in delimiter:
-            delimiter = "".join(map(lambda d: f"\\{d}", delimiter))
-
-        new_nodes = []
-        for node in old_nodes:
-            # Use regex to handle different delimiters using the same character
-            # (e.g. handle italic (*) separately from bold (**))
-            regex = f"(?<!{delimiter}){delimiter}(?!{delimiter})"
-            split_text = re.split(regex, node.text)
-            for text in split_text:
-                if len(text) > 0:
-                    node_type = (
-                        node.text_type if split_text.index(text) % 2 == 0 else text_type
-                    )
-                    new_nodes.append(TextNode(text, node_type))
-
-        return new_nodes
-
-    @staticmethod
-    def extract_markdown_links(text):
-        regex = r"\[.+?\]\(.+?\)"
-        links = re.findall(regex, text)
-        results = []
-        for link in links:
-            anchor_text = link.split("[")[1].split("]")[0]
-            url = link.split("(")[1].split(")")[0]
-            results.append((anchor_text, url))
-        return results
-
-    @staticmethod
-    def extract_markdown_images(text):
-        regex = r"\!\[.+?\]\(.+?\)"
-        images = re.findall(regex, text)
-        results = []
-        for image in images:
-            alt_text = image.split("[")[1].split("]")[0]
-            url = image.split("(")[1].split(")")[0]
-            results.append((alt_text, url))
-        return results
-
-    @staticmethod
-    def split_nodes_link(old_nodes):
-        regex = r"\[.+?\]\(.+?\)"
-        new_nodes = []
-        for node in old_nodes:
-            split_text = re.split(regex, node.text)
-            links = Utils.extract_markdown_links(node.text)
-            for text, link in zip_longest(split_text, links):
-                if text is not None and len(text) > 0:
-                    new_nodes.append(TextNode(text, node.text_type, node.url))
-                if link is not None:
-                    new_nodes.append(TextNode(link[0], TextType.LINK, link[1]))
-        return new_nodes
-
-    @staticmethod
-    def split_nodes_image(old_nodes):
-        regex = r"\!\[.+?\]\(.+?\)"
-        new_nodes = []
-        for node in old_nodes:
-            split_text = re.split(regex, node.text)
-            images = Utils.extract_markdown_images(node.text)
-            for text, image in zip_longest(split_text, images):
-                if text is not None and len(text) > 0:
-                    new_nodes.append(TextNode(text, node.text_type, node.url))
-                if image is not None:
-                    new_nodes.append(TextNode(image[0], TextType.IMAGE, image[1]))
-        return new_nodes
-
-    @staticmethod
-    def text_to_textnodes(text):
-        text_node = TextNode(text, TextType.NORMAL)
-        output = Utils.split_nodes_delimiter([text_node], "*", TextType.ITALIC)
-        output = Utils.split_nodes_delimiter(output, "**", TextType.BOLD)
-        output = Utils.split_nodes_delimiter(output, "`", TextType.CODE)
-        output = Utils.split_nodes_image(output)
-        output = Utils.split_nodes_link(output)
-        return output
 
     @staticmethod
     def markdown_to_blocks(markdown):
